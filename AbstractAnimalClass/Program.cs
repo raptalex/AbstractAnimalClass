5a36663b500be023ece71aad14488be5cd19b455
arapt <arapt@DESKTOP-70PT2F0>
2023-02-27 17:19:25 -0500

    Add project files.

---
+++ AbstractAnimalClass/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+abstract class Animal
+{
+    // Property
+    public abstract string Name { get; set; }
+    // Methods
+    public abstract string Describe();
+    public string whatAmI()
+    {
+        return "I am an animal";
+    }
+}
+
+class Lizard : Animal
+{
+    public enum SexType { Male, Female, None }
+    public override string Name { get; set; }
+    public string Color { get; set; }
+    public SexType Sex { get; set; }
+
+    public Lizard()
+    {
+        Name = string.Empty;
+        Color = string.Empty;
+        Sex = SexType.None;
+    }
+    public Lizard(string _name, string _color, SexType _sex)
+    {
+        Name = _name;
+        Color = _color;
+        Sex = _sex;
+    }
+    public override string Describe()
+    {
+        return "My name is " + Name + ". I am a " + Sex.ToString().ToLower() + " lizard and my skin is " + Color.ToLower() + ".";
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Lizard lizard1 = new Lizard();
+        lizard1.Name = "Mikey";
+        lizard1.Color = "red";
+        lizard1.Sex = Lizard.SexType.Male;
+
+        Console.WriteLine(lizard1.Describe());
+
+        Lizard lizard2 = new Lizard("Rachael", "Turquoise", Lizard.SexType.Female);
+
+        Console.WriteLine(lizard2.Describe());
+    }
+}
